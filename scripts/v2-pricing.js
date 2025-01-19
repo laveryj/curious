3c18aa5a66c6a2d870af5cc98d40f83a416a0ab6
laveryj <105878149+laveryj@users.noreply.github.com>
2025-01-19 12:19:30 +0000

    Adjust pricing code

--- scripts/v2-pricing.js
+++ scripts/v2-pricing.js
@@ -2,17 +2,17 @@ fetch('assets/data/pricing.json')
     .then(response => response.json())
     .then(data => {
         // Calculate per visitor prices
-        const discoverPV = (data.discover / (data.footfall / 12)).toFixed(2);
-        const engagePV = (data.engage / (data.footfall / 12)).toFixed(2);
-        const monetisePV = (data.monetise / (data.footfall / 12)).toFixed(2);
+        const DiscoverPV = (data.Discover / (data.footfall / 12)).toFixed(2);
+        const EngagePV = (data.Engage / (data.footfall / 12)).toFixed(2);
+        const MonetisePV = (data.Monetise / (data.footfall / 12)).toFixed(2);
 
         // Update HTML elements with raw prices and per visitor prices
-        document.getElementById('discover').textContent = `£${data.discover}`;
-        document.getElementById('engage').textContent = `£${data.engage}`;
-        document.getElementById('monetise').textContent = `£${data.monetise}`;
-        document.getElementById('discover-perVisitor').textContent = `${discoverPV}p per visitor`;
-        document.getElementById('engage-perVisitor').textContent = `${engagePV}p per visitor`;
-        document.getElementById('monetise-perVisitor').textContent = `${monetisePV}p per visitor`;
+        document.getElementById('Discover').textContent = `£${data.Discover}`;
+        document.getElementById('Engage').textContent = `£${data.Engage}`;
+        document.getElementById('Monetise').textContent = `£${data.Monetise}`;
+        document.getElementById('Discover-perVisitor').textContent = `${DiscoverPV}p per visitor`;
+        document.getElementById('Engage-perVisitor').textContent = `${EngagePV}p per visitor`;
+        document.getElementById('Monetise-perVisitor').textContent = `${MonetisePV}p per visitor`;
 
         document.getElementById('footfall').textContent = `${data.footfall}`;
     })
